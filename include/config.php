22234b6a21d379f34cfbfbc7fc55ae96bc1ffcae
babahdigital <babahdigital@gmail.com>
2026-01-09 17:05:47 +0000

    Jan 10, 2026, 1:05 AM

--- include/config.php
+++ include/config.php
@@ -1,5 +1,6 @@
 <?php 
 if(substr($_SERVER["REQUEST_URI"], -10) == "config.php"){header("Location:./");}; 
-$data['mikhmon'] = array ('1'=>'mikhmon<|<abdullah','mikhmon>|>mZ2amZOlsZo=');
+$data['mikhmon'] = array ('1'=>'mikhmon<|<kamtib','mikhmon>|>o5KfrJqUaWNl');
 
 $data['LapasBatulicin'] = array ('1'=>'LapasBatulicin!10.10.83.1','LapasBatulicin@|@abdullah','LapasBatulicin#|#mZ2amZOlsZo=','LapasBatulicin%Lapas Batulicin','LapasBatulicin^wartelpas.net','LapasBatulicin&Rp','LapasBatulicin*10','LapasBatulicin(1','LapasBatulicin)','LapasBatulicin=10','LapasBatulicin@!@disable');
+
