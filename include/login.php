723c7d4df6c6f65ab0157f5b88939ee4823daeb8
babahdigital <babahdigital@gmail.com>
2026-01-09 15:49:57 +0000

    Jan 9, 2026, 11:49 PM

--- include/login.php
+++ include/login.php
@@ -27,7 +27,7 @@ session_start();
     </div>
     <div class="card-body">
       <div class="text-center pd-5">
-        <img src="img/favicon.png" alt="MIKHMON Logo">
+        <img src="img/logo.png" alt="BabahDigital Logo">
       </div>
       <div  class="text-center">
       <span style="font-size: 25px; margin: 10px;">MIKHMON</span>
