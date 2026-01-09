ac651fd43f7d40821cee03fb578f59c2bd47e1c8
babahdigital <babahdigital@gmail.com>
2026-01-09 15:53:42 +0000

    Jan 9, 2026, 11:53 PM

--- include/menu.php
+++ include/menu.php
@@ -173,7 +173,7 @@ if($idleto != "disable"){
 
 <div id="navbar" class="navbar">
   <div class="navbar-left">
-    <a id="brand" class="text-center" href="javascript:void(0)">MIKHMON</a>
+    <a id="brand" class="text-center" href="javascript:void(0)">WARTELPAS</a>
 
 <a id="openNav" class="navbar-hover" href="javascript:void(0)"><i class="fa fa-bars"></i></a>
 <a id="closeNav" class="navbar-hover" href="javascript:void(0)"><i class="fa fa-bars"></i></a>
