56e4a03503fb2ca42535a9f64be7aaeba394995b
babahdigital <babahdigital@gmail.com>
2026-01-21 21:53:24 +0000

    Jan 22, 2026, 5:53 AM

--- report/build_sales_summary.php
+++
@@ -1,3 +0,0 @@
-<?php
-// Deprecated: moved to /tools/build_sales_summary.php
-require_once dirname(__DIR__) . '/tools/build_sales_summary.php';
--- report/check_block.php
+++
@@ -1,3 +0,0 @@
-<?php
-// Deprecated: moved to /tools/check_block.php
-require_once dirname(__DIR__) . '/tools/check_block.php';
