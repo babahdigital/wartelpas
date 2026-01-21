87bed239d124eca240f41a001f23eaec44f7bb58
babahdigital <babahdigital@gmail.com>
2026-01-21 09:43:51 +0000

    Jan 21, 2026, 5:43 PM

--- hotspot/users.php
+++ hotspot/users.php
@@ -1702,7 +1702,7 @@ if ($is_ajax) {
         </td>
         <td class="text-center">
           <?php if(!empty($u['relogin'])): ?>
-            <button type="button" class="btn-act btn-act-relogin" data-user="<?= htmlspecialchars($u['name'], ENT_QUOTES) ?>" title="Detail Relogin"><i class="fa fa-eye"></i></button>
+            <button type="button" class="btn-act btn-act-relogin" data-user="<?= htmlspecialchars($u['name'], ENT_QUOTES) ?>" data-blok="<?= htmlspecialchars($u['blok'], ENT_QUOTES) ?>" data-profile="<?= htmlspecialchars($u['profile'], ENT_QUOTES) ?>" title="Detail Relogin"><i class="fa fa-eye"></i></button>
           <?php endif; ?>
           <?php if (in_array($req_status, ['all','used','rusak','online'], true)): ?>
             <?php if (strtoupper($u['status']) === 'TERPAKAI' && in_array($req_status, ['all','used'], true)): ?>
