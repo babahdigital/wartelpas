c5ca7f80dea3664b3db96f400a6a5aaf01aee19d
babahdigital <babahdigital@gmail.com>
2026-01-25 03:35:20 +0000

    Jan 25, 2026, 11:35 AM

--- dashboard/aload.php
+++ dashboard/aload.php
@@ -557,7 +557,7 @@ if ($load == "hotspot") {
             if(typeof Highcharts !== 'undefined') {
                 var catLen = <?= count($categories) ?>;
                 Highcharts.chart('chart_income_stat', {
-                    chart: { backgroundColor: 'transparent', plotBackgroundColor: 'transparent', type: 'area', spacingBottom: 0, reflow: true, zoomType: 'xy', height: null, borderWidth: 0, spacing: [10, 0, 10, 0], marginLeft: 0, marginRight: 0 },
+                    chart: { backgroundColor: 'transparent', plotBackgroundColor: 'transparent', type: 'area', spacingBottom: 0, reflow: true, zoomType: 'xy', height: null, borderWidth: 0, spacing: [10, 0, 10, 0], marginLeft: 10, marginRight: 10 },
                     title: { text: null },
                     xAxis: {
                         categories: <?= $jsonCategories ?>,
