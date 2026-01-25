41121700ae6fbb6d160256da03fc0b51ffceabf3
babahdigital <babahdigital@gmail.com>
2026-01-25 12:09:32 +0000

    Jan 25, 2026, 8:09 PM

---
+++ report/whatsapp/config.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'db_file' => dirname(__DIR__, 2) . '/db_data/mikhmon_stats.db',
+    'pdf_dir' => dirname(__DIR__) . '/pdf',
+    'log_limit' => 50,
+    'timezone' => 'Asia/Makassar',
+    'wa' => [
+        'endpoint_send' => 'https://api.fonnte.com/send',
+        'token' => '',
+        'country_code' => '62'
+    ]
+];
