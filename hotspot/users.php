45a139ef33d8c623958d85bb101a43f7eb0c2159
babahdigital <babahdigital@gmail.com>
2026-01-25 03:20:39 +0000

    Jan 25, 2026, 11:20 AM

--- hotspot/users.php
+++ hotspot/users.php
@@ -2691,7 +2691,7 @@ if ($debug_mode && !$is_ajax) {
                         <?php if ($is_used && in_array($req_status, ['all','used'], true)): ?>
                           <button type="button" class="btn-act btn-act-print" onclick="window.open('./report/print_rincian.php?mode=usage&status=used&user=<?= urlencode($u['name']) ?>&session=<?= $session ?>','_blank').print()" title="Print Bukti Pemakaian"><i class="fa fa-print"></i></button>
                         <?php elseif ($is_online && in_array($req_status, ['all','online'], true)): ?>
-                          <button type="button" class="btn-act btn-act-print" onclick="window.open('./report/print_rincian.php?mode=usage&status=online&user=<?= urlencode($u['name']) ?>&session=<?= $session ?>','_blank').print()" title="Print Rincian Online"><i class="fa fa-print"></i></button>
+                          <button type="button" class="btn-act btn-act-print" onclick="window.open('./hotspot/print.used.php?user=<?= urlencode($u['name']) ?>&session=<?= $session ?>','_blank')" title="Print Bukti Pemakaian"><i class="fa fa-print"></i></button>
                         <?php elseif ($is_rusak && in_array($req_status, ['all','rusak'], true)): ?>
                           <button type="button" class="btn-act btn-act-print" onclick="window.open('./report/print_rincian.php?mode=usage&status=rusak&user=<?= urlencode($u['name']) ?>&session=<?= $session ?>','_blank').print()" title="Print Rincian Rusak"><i class="fa fa-print"></i></button>
                         <?php elseif ($is_retur && in_array($req_status, ['all','retur'], true)): ?>
