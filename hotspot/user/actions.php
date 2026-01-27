dbdbe334749381f0ca7122b86d950eb22de02da4
babahdigital <babahdigital@gmail.com>
2026-01-27 19:00:58 +0000

    Jan 28, 2026, 3:00 AM

--- hotspot/user/actions.php
+++ hotspot/user/actions.php
@@ -3,6 +3,7 @@
 if (isset($_GET['action']) || isset($_POST['action'])) {
   $root_dir = dirname(__DIR__, 2);
   require_once($root_dir . '/include/acl.php');
+  require_once(__DIR__ . '/helpers.php');
   $env = [];
   $envFile = $root_dir . '/include/env.php';
   if (file_exists($envFile)) {
